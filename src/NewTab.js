3cc4effefb9cbd4d8b24f4003ce1333db05a7682
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-02-02 23:54:56 +0300

    update

--- src/NewTab.js
+++ src/NewTab.js
@@ -1,38 +1,73 @@
-function logItems(bookmarkItem, folder) {
-    if (bookmarkItem.url) {
-        var bookmarkNode = document.createElement('li');
-        bookmarkNode.id = bookmarkItem.id;
-        bookmarkNode.title = bookmarkItem.title;
-        var url = document.createElement('a');
-        url.href = bookmarkItem.url;
-        url.text = bookmarkItem.title;
-        bookmarkNode.appendChild(url);
-        document.getElementById(folder).appendChild(bookmarkNode);
+(async function () {
+    browser.bookmarks.onCreated.addListener(displayBookmarkTree);
+    browser.storage.local.get('bookmarks').then(
+        () => {
+            browser.bookmarks.getTree().then(
+                displayBookmarkTree, onRejected
+            );
+        });
+    /*
+    (object) => {
+    document.getElementById('bookmarks').innerHTML = object.bookmarks;
+    },
+    */
+})()
+
+
+function displayBookmarkTree(bookmarkTree) {
+    createBookmarkNode(bookmarkTree[0]);
+    saveState();
+}
+
+function saveState() {
+    browser.storage.local.set(
+        { bookmarks: document.getElementById('bookmarks').innerHTML }
+    );
+}
+
+function createBookmarkNode(bookmarkItem, folder) {
+    var id = bookmarkItem.id;
+    var title = bookmarkItem.title;
+    var url = bookmarkItem.url;
+
+    if (!folder) {
+        id = 'bookmarks';
+    } else if (url) {
+        var node = document.createElement('li');
+        node.id = id;
+        node.title = title;
+        var link = document.createElement('a');
+        link.href = url;
+        link.text = title;
+        node.appendChild(link);
+        document.getElementById(folder).appendChild(node);
     } else {
-        var bookmarkNode = document.createElement('ul');
-        bookmarkNode.appendChild(document.createTextNode(bookmarkItem.title));
-        bookmarkNode.id = bookmarkItem.id;
-        bookmarkNode.text = bookmarkItem.title;
-        bookmarkNode.title = bookmarkItem.title;
-        document.getElementById(folder).appendChild(bookmarkNode);
-        if (bookmarkItem.children) {
-            for (child of bookmarkItem.children) {
-                logItems(child, bookmarkItem.id);
-            }
+        var node = document.createElement('ul');
+        node.id = id;
+        node.title = title;
+        var link = document.createElement('a');
+        link.href = '#' + title;
+        link.text = title;
+        link.title = id;
+        link.onclick = event => {
+            document.getElementById(event.target.title).classList.toggle('hideChildren');
+        };
+        node.appendChild(link);
+        document.getElementById(folder).appendChild(node);
+    }
+
+    //console.log(folder + ' / ' + title + ' / ' + type + ' / ' + id);
+    if (bookmarkItem.children) {
+        for (child of bookmarkItem.children) {
+            createBookmarkNode(child, id);
         }
     }
+
+
 }
 
 function onRejected(error) {
     console.log(`An error: ${error}`);
 }
 
-function logTree(bookmarkItems) {
-    browser.bookmarks.getTree();
-    logItems(bookmarkItems[0], 'bookmarks');
-}
 
-; (async function () {
-    var gettingTree = browser.bookmarks.getTree();
-    gettingTree.then(logTree, onRejected);
-})()
