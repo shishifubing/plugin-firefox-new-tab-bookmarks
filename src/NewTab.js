ee858cc919f182bdb2e0e9eb553e0574e32d144f
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-02-03 03:33:18 +0300

    update

--- src/NewTab.js
+++ src/NewTab.js
@@ -28,38 +28,45 @@ function saveState() {
 function createBookmarkNode(bookmarkItem, folder) {
     var id = bookmarkItem.id;
     var title = bookmarkItem.title;
+    var tag = 'li';
     var url = bookmarkItem.url;
+    if (!url) {
+        url = '#' + title;
+        tag = 'ul';
+    }
+    if (!title) {
+        title = url
+    };
 
     if (!folder) {
         id = 'bookmarks';
-    } else if (url) {
-        var node = document.createElement('li');
+    } else {
+        var node = document.createElement(tag);
         node.id = id;
         node.title = title;
+        if (folder !== 'bookmarks') {
+            node.className = 'hideChildren';
+        }
         var link = document.createElement('a');
         link.href = url;
         link.text = title;
-        node.appendChild(link);
-        document.getElementById(folder).appendChild(node);
-    } else {
-        var node = document.createElement('ul');
-        node.id = id;
-        node.title = title;
-        var link = document.createElement('a');
-        link.href = '#' + title;
-        link.text = title;
         link.title = id;
-        link.onclick = event => {
-            document.getElementById(event.target.title).classList.toggle('hideChildren');
-        };
+        if (tag === 'ul') {
+            link.onclick = event => {
+                document.getElementById(event.target.title).classList.toggle('hideChildren');
+            };
+        }
         node.appendChild(link);
         document.getElementById(folder).appendChild(node);
     }
 
-    //console.log(folder + ' / ' + title + ' / ' + type + ' / ' + id);
     if (bookmarkItem.children) {
-        for (child of bookmarkItem.children) {
-            createBookmarkNode(child, id);
+        if (bookmarkItem.children.length !== 0) {
+            for (child of bookmarkItem.children) {
+                createBookmarkNode(child, id);
+            }
+        } else {
+            document.getElementById(id).style.display = 'none';
         }
     }
 
