38ad7f2670fdf12a1c7c69fec785b590d138d0e4
jingyangzhenren <97828377+jingyangzhenren@users.noreply.github.com>
2021-02-04 19:57:20 +0300

    update

--- src/NewTab.js
+++ src/NewTab.js
@@ -1,27 +1,25 @@
 (async function () {
-    browser.bookmarks.onCreated.addListener(displayBookmarkTree);
-    browser.storage.local.get('bookmarks').then(
-        () => {
-            browser.bookmarks.getTree().then(
-                displayBookmarkTree, onRejected
-            );
-        });
-    /*
-    (object) => {
-    document.getElementById('bookmarks').innerHTML = object.bookmarks;
-    },
-    */
+    /*browser.storage.local.get('bookmarks').then(
+        (object) => {
+            document.getElementById('bookmarks') = object.bookmarks;
+        },
+        
+        });*/
+    browser.bookmarks.getTree().then(
+        displayBookmarkTree, onRejected
+    );
 })()
 
 
-function displayBookmarkTree(bookmarkTree) {
-    createBookmarkNode(bookmarkTree[0]);
-    saveState();
+function displayBookmarkTree(bookmarkItemTree) {
+    var bookmarkTree = createBookmarkNode(bookmarkItemTree[0]);
+    document.getElementById('main').appendChild(bookmarkTree);
+    //saveBookmarkTree(bookmarkTree);
 }
 
-function saveState() {
+function saveBookmarkTree(bookmarkTree) {
     browser.storage.local.set(
-        { bookmarks: document.getElementById('bookmarks').innerHTML }
+        { bookmarks: bookmarkTree.outerHTML }
     );
 }
 
@@ -38,13 +36,17 @@ function createBookmarkNode(bookmarkItem, folder) {
         title = url
     };
 
+    var node = null;
     if (!folder) {
+        node = document.createElement('div');
         id = 'bookmarks';
+        node.id = 'bookmarks';
+        node.title = 'bookmarks';
     } else {
-        var node = document.createElement(tag);
+        node = document.createElement(tag);
         node.id = id;
         node.title = title;
-        if (folder !== 'bookmarks') {
+        if (folder.id !== 'bookmarks') {
             node.className = 'hideChildren';
         } else {
             node.classList.add('rootFolder');
@@ -54,34 +56,40 @@ function createBookmarkNode(bookmarkItem, folder) {
         link.href = url;
         link.title = id;
         if (tag === 'ul') {
+            if (!node.classList.contains('rootFolder')) {
+                node.onclick = event => {
+                    event.target.classList.toggle('hideChildren');
+                };
+            }
             link.className = 'folderLink';
             link.onclick = event => {
                 document.getElementById(event.target.title).classList.toggle('hideChildren');
+                console.log(event.target.title);
             };
             var icon = document.createElement('img');
             icon.src = '../icons/folder_arrow.png';
             icon.width = '10';
             icon.height = '10';
             icon.alt = 'folder_arrow';
-            icon.padding = '0 5px 0 0';
-            //link.appendChild(icon);
+            icon.margin = '0 3px 0 0';
+            icon.title = id;
+            //icon.onclick = link.onclick;
+            link.appendChild(icon);
         }
         link.appendChild(text);
         node.appendChild(link);
-        document.getElementById(folder).appendChild(node);
+        folder.appendChild(node);
     }
-
     if (bookmarkItem.children) {
         if (bookmarkItem.children.length !== 0) {
             for (child of bookmarkItem.children) {
-                createBookmarkNode(child, id);
+                createBookmarkNode(child, node);
             }
         } else {
-            document.getElementById(id).style.display = 'none';
+            node.style.display = 'none';
         }
     }
-
-
+    return node;
 }
 
 function onRejected(error) {
